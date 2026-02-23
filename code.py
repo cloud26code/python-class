cfd9046a1882b6a6d73fd64ca4e6e8a6624f51a7
cloud26code <fatimaahmad0712@gmail.com>
2026-02-22 17:36:06 -0800

    important file

---
+++ code.py
@@ -0,0 +1,29 @@
+#Comments
+#this code s replacing empty space
+
+#variables understandng
+# user_age = 22
+# User_age = 44
+#
+# user_age = 33
+# a = 12
+# age = 12
+# student_age = 20
+
+
+# name = "zaheen"
+#print(user_age,User_age)
+
+## data types
+#Numberic
+#integer: 23424, -3,0,-234
+#float:234.098, -23.0, -1.0
+#text 
+# string: "we are learning datatyes", 'abc gardan town', '''text''', """text"""
+#boolean: True,False
+
+##Collections
+#list: cloud_class_student = ["ali", "hamza","abc","aaa"]
+#tuple:  cloud_class =("ali", "hamza","abc","aaa")
+#Dictionary: student_data ={'name':"ali",'age':17,"address":"abc street multan"}
+#set:{12,14,12}
