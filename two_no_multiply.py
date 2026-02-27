77a0d1e92345f5ffd89803aa3f2709bc04d195de
cloud26code <fatimaahmad0712@example.com>
2026-02-27 14:01:27 +0500

    Multiplication

---
+++ two_no_multiply.py
@@ -0,0 +1,18 @@
+# write a program that get two integer values fom the user
+# and multiply to output result.
+
+num11 = int(input("enter your choice"))
+num12 = int(input("enter your choice"))
+multiply = num11 * num12
+print("the result of multiplication is:",multiply)
+
+
+num13 = int(input("enter your choice"))
+num14 = int(input("enter your choice"))
+mulpitly = num13 * num14
+print("the result of multiplication is:",multiply)
+
+num101 = float(input("enter your choice"))
+num102 =float(input("enter your choice"))
+multiply = num101 * num102 
+print("the result of multiplication is:",multiply)
