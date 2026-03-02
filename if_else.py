5fc337bde1622f96aaea9c57db493ed9195bf0fb
cloud26code <fatimaahmad0712@example.com>
2026-03-02 15:19:33 +0500

    if,if-else

---
+++ if_else.py
@@ -0,0 +1,39 @@
+#num = float(input("enter a number"))
+#if num >20:
+#  print("answer is true")
+
+   
+#num = int(input("enter a marks"))
+#if num >70:
+#  print("congurlation you're pass")
+
+#age = float(input("enter a age"))
+#if age > 21:
+# print("you are qualified for the job")
+
+age = int(input("enter a age"))
+if age>22:
+    print("you're qualified")
+else:
+    print("you're not qualified")
+
+
+num = int(input("enter a number of your choice"))
+if num>0:
+    print("number are positive")
+else:
+    print("number are negative")    
+
+num = float(input("enter a number"))
+if num <45:
+    print("answer is true")
+else:
+    print("answer is false") 
+
+student_marks = float(input("enter a student marks"))
+if student_marks >900:
+    print("you got this marks,your admission are confirm")
+else:
+    print("you not got this marks,your admission are not confirm")            
+
+    
